81c1f2776a951cf2fdfd6b01e8187fbbd9cfe21b
Gustavo O. Barbosa <104173191+souguxtavo@users.noreply.github.com>
2023-06-27 22:38:24 +0000

    Tue, Jun 27, 2023, 7:38 PM -03:00

--- pluralidade/pluraridade.c
+++ pluralidade/pluraridade.c
@@ -8,26 +8,94 @@
 // Candidatos possuem nome e contagem de votos
 typedef struct
 {
-string nome;
-int votos;
+    string name;
+    int votes;
 }
-candidato;
+candidate;
 
 // Array de candidatos
-candidato candidatos[MAX];
+candidate candidates[MAX];
 
 // Número de candidatos
-int quantidade_candidatos;
+int candidate_count;
 
 // Protótipos de função
-bool votar(string nome);
-void imprimir_vencedor(void);
+bool vote(string name);
+void print_winner(void);
 
 int main(int argc, string argv[])
 {
-// Verificar uso inválido
-if (argc < 2)
+    // Verificar uso inválido
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Preencher o array de candidatos
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Percorrer todos os eleitores
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Verificar voto inválido
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Exibir o vencedor da eleição
+bool vote(string name)
 {
-printf("Uso: plurality [candidato...]\n");
-return 1;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Atualizar a contagem de votos com base em um novo voto
+void print_winner(void)
+{
+    int max_votes = candidates[0].votes;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    return;
 }
